2dcb851c05e18d49a03a6075d517680f770d1996
c-dev256 <39969850+c-dev256@users.noreply.github.com>
2025-03-29 01:03:49 -0400

    Spinny trianglegit add .!

--- main.py
+++ main.py
@@ -3,14 +3,19 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     running = True
     while running:
@@ -18,7 +23,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        pygame.Surface.fill(screen,(255,255,255))    
+        pygame.Surface.fill(screen,(0,0,0))    
+        dt = clock.tick(60) / 1000
+        
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
 if __name__ == "__main__":
         main()
